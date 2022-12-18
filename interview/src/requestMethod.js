d91076adcd16cfa0e34cfef7c30f3253a573b5a6
Mohit agarawal <2019285@iiitdmj.ac.in>
2022-12-18 15:13:30 +0530

    .

--- interview/src/requestMethod.js
+++ interview/src/requestMethod.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 let BASE_URL = "http://localhost:8800/api";
 if(process.env.NODE_ENV ==="production"){
-	BASE_URL = "https://interview-scheduler7.herokuapp.com/api";
+	BASE_URL = "https://interviewscheduler.cyclic.app/api";
 }
 
 export const publicRequest = axios.create({ baseURL: BASE_URL })
